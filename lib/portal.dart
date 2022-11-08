e6b147691406f29866fad48ece88ccf0b8212935
Choyon Chandra Bonik <choyonbonik@gmail.com>
2022-11-08 20:19:02 +0600

    NewsApp

---
+++ lib/portal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Portal extends StatefulWidget {
+  const Portal({Key? key}) : super(key: key);
+
+  @override
+  State<Portal> createState() => _PortalState();
+}
+
+class _PortalState extends State<Portal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
